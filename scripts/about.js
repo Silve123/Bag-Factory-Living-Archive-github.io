93a1c79c21eebad85f9197f8ff84f6a8b8e4cea7
headmaster <headmaster@headmaster.headmaster>
2024-04-20 16:28:56 +0200

    z-index fixed for publication, about and timeline

--- scripts/about.js
+++ scripts/about.js
@@ -1,14 +1,21 @@
-function updateSidebarStatus() {
+function updateSidebarStatusAndTopBar() {
     var isSidebarOpen = localStorage.getItem('sidebarOpen');
+    var isTopbarArtistsOpen = localStorage.getItem('topBarArtistsOpen');
 
     if (isSidebarOpen) {
         document.querySelector('.fixed-sidebar-object').classList.add('sidebar-open');
     } else {
         document.querySelector('.fixed-sidebar-object').classList.remove('sidebar-open');
     }
+
+    if (isTopbarArtistsOpen) {
+        document.querySelector('.fixed-topbar-object').classList.add('topbar-artists-open');
+    } else {
+        document.querySelector('.fixed-topbar-object').classList.remove('topbar-artists-open');
+    }
 }
 
-updateSidebarStatus();
-setInterval(updateSidebarStatus, 10);
+updateSidebarStatusAndTopBar();
+setInterval(updateSidebarStatusAndTopBar, 10);
 
 
